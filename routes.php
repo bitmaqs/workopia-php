1a79bf5db911bd831c1239f2d63c386219652699
mqqs <17243060+mqqs@users.noreply.github.com>
2024-02-07 19:50:04 -0300

    Single listing and Named params

--- routes.php
+++ routes.php
@@ -3,3 +3,4 @@
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/', 'controllers/listings/create/create.php');
+$router->get('/listing', 'controllers/listings/show.php');
