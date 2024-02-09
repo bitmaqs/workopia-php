dcd3aeddb6894c026dcf4763e521ba12b7fbb98d
mqqs <17243060+mqqs@users.noreply.github.com>
2024-02-09 11:29:37 -0300

    add comments

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -13,6 +13,11 @@ class ListingController
         $this->db = new Database($config);
     }
 
+    /**
+     * Show all listings
+     * 
+     * @return void
+     */
     public function index()
     {
         $listings = $this->db->query("SELECT * FROM listings")->fetchAll();
@@ -24,10 +29,23 @@ class ListingController
             ]
         );
     }
+
+    /**
+     * Show the create listing form
+     * 
+     * @return void
+     */
     public function create()
     {
         loadView("listings/create");
     }
+
+    /**
+     * Show a single listing
+     * 
+     * @param array $params
+     * @return void
+     */
     public function show()
     {
         $id = $_GET['id'] ?? '';
