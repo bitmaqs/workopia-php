639f03540e2c727779284da4179a3661b129b0fb
mqqs <17243060+mqqs@users.noreply.github.com>
2024-02-13 19:23:12 -0300

    Flash message methods

--- App/Controllers/ListingController.php
+++ App/Controllers/ListingController.php
@@ -125,6 +125,9 @@ class ListingController
 
             $this->db->query($query, $newListingData);
 
+            Session::setFlashMessage('success_message', 'Listing created successfully');
+
+
             redirect('/listings');
         }
     }
@@ -151,14 +154,15 @@ class ListingController
         }
         // Authorization
         if (!Authorization::isOwner($listing->user_id)) {
-            $_SESSION['error_message'] = 'You are not authorized to delete this listing';
+            Session::setFlashMessage('error_message', 'You are not authorized to delete this listing');
             return redirect('/listings/' . $listing->id);
         }
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params);
 
         // Set a flash message
-        $_SESSION['success_message'] = 'Listing deleted successfully';
+        Session::setFlashMessage('success_message', 'Listing deleted successfully');
+
 
         redirect('/listings');
     }
@@ -249,7 +253,8 @@ class ListingController
             $updateValues['id'] = $id;
             $this->db->query($updateQuery, $updateValues);
 
-            $_SESSION['success_message'] = 'Listing updated';
+            // Set a flash message
+            Session::setFlashMessage('success_message', 'Listing updated successfully');
 
             redirect('/listings/' . $id);
         }
